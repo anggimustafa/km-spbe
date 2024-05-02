34232b6693e4c8a61877b7d7e4642364bf22455e
Anggi Mustafa <anggimustafaa@gmail.com>
2024-05-02 08:25:35 +0700

    menambahkan modal pilih author

--- km-spbe/resources/views/layouts/app.blade.php
+++ km-spbe/resources/views/layouts/app.blade.php
@@ -205,6 +205,17 @@
             }
         });
     </script>
+    <script>
+        new DataTable('#example-modal', {
+            layout: {
+                bottomEnd: {
+                    paging: {
+                        boundaryNumbers: false
+                    }
+                }
+            }
+        });
+    </script>
     <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
     <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../assets/js/sidebarmenu.js"></script>
